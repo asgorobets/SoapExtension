60bd354fec3477d4fd03d9d12076525bc1300ea0
Alexei Gorobets <asgorobets@gmail.com>
2016-02-24 16:15:39 +0200

    Initial commit

---
+++ src/ServiceContainer/SoapExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Behat\SoapExtension\ServiceContainer;
+
+use Behat\Testwork\ServiceContainer\Extension as ExtensionInterface;
+use Behat\Testwork\ServiceContainer\ExtensionManager;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Soap extension for Behat.
+ */
+class SoapExtension implements ExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigKey()
+    {
+        return 'soap';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(ExtensionManager $extensionManager)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure(ArrayNodeDefinition $builder)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ContainerBuilder $container, array $config)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+    }
+}
